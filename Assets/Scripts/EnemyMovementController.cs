9b0004ad6b2b19596c4dddd302039112f7956f4f
HuanLe <lehuannhatrang98@gmail.com>
2017-10-26 16:06:54 +0700

    tạo temp scence for huân Đang viết AI cho quái

---
+++ Assets/Scripts/EnemyMovementController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovementController : MonoBehaviour {
+    public float enemySpeed;
+    Rigidbody2D enemyRB;
+    Animator enemyAnimation;
+    // Use this for initialization
+    public GameObject enemyGraphic;
+    bool facingRight = true;
+    float facingTime=5f;
+    float nexFlip=0f;
+    bool canFlip = true;
+
+    void Awake() {
+        enemyRB = GetComponent<Rigidbody2D>();
+        enemyAnimation = GetComponentInChildren<Animator>();
+
+    }
+
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Time.time > nexFlip)
+        {
+            nexFlip = Time.time + facingTime;
+            Flip();
+        }
+	}
+
+    void Flip() {
+        facingRight = !facingRight;
+        Vector3 theScale = enemyGraphic.transform.localScale;
+        theScale.x *= -1;
+        enemyGraphic.transform.localScale = theScale;
+    }
+}
