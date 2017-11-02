71c000beb940cba6feddaafc626186f4f7c16d45
HuanLe <lehuannhatrang98@gmail.com>
2017-11-03 00:04:56 +0700

    Máu cho nhân vật và enemyDamage

---
+++ Assets/Scripts/EnemyDamage.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyDamage : MonoBehaviour {
+    float damrate = 0.5f;
+    float nextdam ;
+    public float pushBackForce;
+    // Use this for initialization
+    void Start () {
+        nextdam = 0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag=="Main Character" && nextdam<Time.time)
+        {           
+            PlayerHealth myPlayerHealth = other.gameObject.GetComponent<PlayerHealth>();
+            myPlayerHealth.addDamage(true);
+            nextdam = damrate + Time.time;
+           
+        }
+        
+    }
+
+}
