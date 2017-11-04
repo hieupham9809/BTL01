694295b7e598931f24322ee44fb3630fc7b6c880
hieupham <hieupham9809@gmail.com>
2017-11-04 13:08:31 +0700

    demo Menu start game and select level

---
+++ Assets/Scripts/Controller Scrips/LevelMenuController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelMenuController : MonoBehaviour {
+
+	public void PlayGame(){
+		Application.LoadLevel ("Huan_tempscence");
+	}
+
+	public void BackToMenu(){
+		Application.LoadLevel ("MainMenu");
+	}
+}
