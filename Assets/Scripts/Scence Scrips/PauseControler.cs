edfbea09a9bf46365ff71e531243314e2aa3ac29
hieupham <hieupham9809@gmail.com>
2017-11-09 02:25:41 +0700

    edit mainmenu + create pause menu

---
+++ Assets/Scripts/Scence Scrips/PauseControler.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+ 
+
+public class PauseControler : MonoBehaviour {
+
+	// Use this for initialization
+
+	public Transform canvas;
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			Pause ();
+		}
+	}
+	public void Pause(){
+		if (canvas.gameObject.activeInHierarchy == false) {
+			canvas.gameObject.SetActive (true);
+			Time.timeScale = 0;
+		} else {
+			canvas.gameObject.SetActive (false);
+			Time.timeScale = 1;	
+		}
+	}
+
+	public void MainMenu(){
+		Application.LoadLevel ("MainMenu");
+	}
+
+	public void Quit(){
+		Application.Quit ();
+	}
+}
