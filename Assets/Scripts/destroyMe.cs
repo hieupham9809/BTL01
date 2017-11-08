e554563c754c68201e15a5db1fa64f91e42b883b
hao luong <haophamluong@gmail.com>
2017-11-09 01:39:00 +0700

    bullet + fire

---
+++ Assets/Scripts/destroyMe.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class destroyMe : MonoBehaviour {
+
+    public float alivetime;
+
+	// Use this for initialization
+	void Start () {
+        Destroy(gameObject, alivetime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
