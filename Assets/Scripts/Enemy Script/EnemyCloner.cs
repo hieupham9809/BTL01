bde703451c7e79b87ee30a3a693652a1ce8fc2ea
HuanLe <lehuannhatrang98@gmail.com>
2017-11-20 11:22:51 +0700

    level boss

---
+++ Assets/Scripts/Enemy Script/EnemyCloner.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyCloner : MonoBehaviour {
+    public GameObject zoombie;
+    public float cloneRate;
+    float nextClone;
+	// Use this for initialization
+	void Start () {
+        nextClone = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Time.time > nextClone)
+        {
+            Instantiate(zoombie, transform.position, transform.rotation);
+            nextClone = Time.time + cloneRate;
+        }
+	}
+}
